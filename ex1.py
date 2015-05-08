38066300bc23ee4d9aa1bca0c92e92baf457d87d
xueweihan <595666367@qq.com>
2015-05-08 13:32:03 +0800

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,9 +1,12 @@
-#coding=utf-8
-a='你好'
-print  a
+#coding:utf-8
+print  "hello Word!"
+print  u"你好？"
 print "Heloo Again"
 print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+
+# 输出中文的时候要在字符串前面加u。
+# 因为python中字符串默认采用ASCII，要声明为unicode就要加u
