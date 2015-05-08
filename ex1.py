be7a8249d2efacbbd7d2738f6d5a0a59b248a1c0
xueweihan <595666367@qq.com>
2015-05-08 12:44:33 +0800

    ex1

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+a='你好'
+print  a
+print "Heloo Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
