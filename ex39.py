38cf1c00e4340f968e10f79b13c8baa93dc77995
xueweihan <595666367@qq.com>
2015-05-21 22:24:31 +0800

    Dictionary

---
+++ ex39.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+#################
+#习题39:可爱的字典
+#################
+# 前言
+#
+# 区别列表和字典：
+# 1.列表：['apple', 'orange'],只可以用数字来索引元素
+# 2.字典：['name': xue, 'age': 22]，不仅可以通过数字索引，还可以通过字符串索引
+
+# create a mapping of state to abbreviation(缩写)
+states = {
+    'Oregon': 'OR'
+    'Florida': 'FL'
+    'California': 'CA'
+}
