95d2082e776a82c879fe1a9320d80ca8f8812cd4
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 21:58:38 +0800

    字符串和文本

--- ex6.py
+++ ex6.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 #################
-#习题6:字符串和文本
+# 习题6:字符串和文本
 #################
-#前言
+# 前言
 #
-#输出文本的时候，最好用到用变量去代替，这样会剩下很多工作
+# 输出文本的时候，最好用到用变量去代替，这样会剩下很多工作
 #
 
 x = "There are %d typs of people." % 10
@@ -18,19 +18,17 @@ print y
 print "I said: %r." % x
 print "I also said: '%s'." % y
 
-hilarious = False #爆笑
-joke_evaluation = "Isn't that joke so funny?! %r"#笑话评估
+hilarious = False # 爆笑
+joke_evaluation = "Isn't that joke so funny?! %r"# 笑话评估
 
 print joke_evaluation % hilarious
+
 w = "This is the left side of....."
 e = "a string with a right side."
 
 print w + e
 
-#笔记
+# 笔记
 #
-#%r和%s的区别
-#
-#%r输出字符串多一个''
-#
-#%r会显示原始数据，用来调试
+# %r和%s的区别
+# %r会显示原始数据，用来调试
