afe6068d5130acda42a33b96315130632debb7dc
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 22:25:50 +0800

    更多打印

--- ex7.py
+++ ex7.py
@@ -1,16 +1,16 @@
 #coding:utf-8
 #################
-#习题7:更多打印
+# 习题7:更多打印
 #################
-#前言
+# 前言
 #
-#巩固练习输出，啦啦啦啦
+# 巩固练习输出，啦啦啦啦
 #
 
-print "Mary had a little lamb"#羊羔
+print "Mary had a little lamb"# 羊羔
 print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."#羊毛
-#“.”输出了十次
+print "And everywhere that Mary went."# 羊毛
+# “.”输出了十次
 print "." * 10 # what 'd that do?
 
 end1 = "C"
@@ -26,8 +26,8 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#watch that comma(逗号) at the end. try removing it to see what happens
-#在下面一行加逗号输出结果：在一行，中间用一个空格隔开
-#不加逗号的结果：分别输出在两行
+# watch that comma(逗号) at the end. try removing it to see what happens
+# 在下面一行加逗号输出结果：在一行，中间用一个空格隔开
+# 不加逗号的结果：分别输出在两行
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
