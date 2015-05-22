878623e4f797fe2c71d1bdee861303b292898392
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 21:52:06 +0800

    更多变量和打印

--- ex5.py
+++ ex5.py
@@ -1,12 +1,12 @@
 #coding:utf-8
 #################
-#习题5:更多的变量和打印
+# 习题5:更多的变量和打印
 #################
-#前言
+# 前言
 #
-#格式化字符串主要作用是：
+# 格式化字符串主要作用是：
 #
-#使用专门的格式和语法把变量的内容放到字符串里。
+# 使用专门的格式和语法把变量的内容放到字符串里。
 #
 # %d输出数字 
 # %s输出字符串
@@ -14,8 +14,8 @@
 
 my_name = 'XueWeiHan'
 my_age = 22
-my_height = 170 #cm
-my_weight = 60 #kg
+my_height = 170 # cm
+my_weight = 60 # kg
 my_eyes = 'Brown'
 my_teeth = 'White'
 my_hair = 'Black'
@@ -24,10 +24,10 @@ print "Let's talk about %s." % my_name
 print "He's %d cm tall." % my_height
 print "He's %d kg heavy." % my_weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes,my_hair)
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
-#这里是有错误的，找出错误
+# 这里是有错误的，找出错误
 print "If I add %d,%d,and %d I get %d" %(
     my_age, my_height, my_weight, my_age + my_height + my_weight)  
-#我没找到错误！
+# 我没找到错误！
