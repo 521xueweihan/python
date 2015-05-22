eb3ae2f027a8fcdcb8fc7751cdbb9075001e11ff
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 22:34:07 +0800

    打印，打印，打印

--- ex9.py
+++ ex9.py
@@ -1,19 +1,20 @@
 #coding:utf-8
 #################
-#习题9:打印，打印，打印
+# 习题9:打印，打印，打印
 #################
-#前言
+# 前言
 #
-#巩固练习打印
+# 巩固练习打印
 #
 
-#Here's some new strange stuff,remember type it exactly.(这是一些新的奇怪的东西)
+# Here's some new strange stuff,remember type it exactly.
+# (这是一些新的奇怪的东西)
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days:", days
-print "Here are the months:",months
+print "Here are the months:", months
 
 print """
 There's something going on here.
@@ -22,10 +23,9 @@ We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
 
-#笔记
+# 笔记
 #
-#\n是换行
-#
-#三个双引号包含的内容可以是多行存在的
-#
-#
+# 1.\n是换行
+# 2.三个双引号包含的内容可以是多行存在的
+# 3.少用""""""这个不优雅
+
