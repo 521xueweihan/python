7700e223a6d959e3be69ffbd4bb39a78a0478966
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 22:41:30 +0800

    那是什么

--- ex10.py
+++ ex10.py
@@ -1,16 +1,18 @@
 #coding:utf-8
 #################
-#习题10:那是什么
+# 习题10:那是什么
 #################
-#前言
+# 前言
 #
-#这里主要理解：转义字符的做用
+# 这里主要理解：转义字符的做用
 #
 
-tabby_cat = "\t I'm tabbed in."
+tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
+# \t是输出八个空格
+# \n是换行
 fat_cat = """
 I'll do a list:
 \t* Cat food
@@ -22,7 +24,7 @@ print persian_cat
 print backslash_cat
 print fat_cat
 
-#
+# 试试运行下面的代码
 #while True:
 #    for i in ["/","-","|","\\","|"]:
 #	print "%s\r" % i,
