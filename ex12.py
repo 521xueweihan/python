552de630ba78bf9a81ec309634633cf19579e7ae
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 22:48:04 +0800

    提示别人

--- ex12.py
+++ ex12.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 #################
-#习题12:提示别人
+# 习题12:提示别人
 #################
-#前言
+# 前言
 #
-#这里主要理解：raw_input("提示信息")的用法
+# 这里主要理解：raw_input("提示信息")的用法
 #
 
 age = raw_input("How old are you? ")
@@ -12,4 +12,4 @@ height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weight? ")
 
 print "So, you are %r old, %r tall and %r heavy."% (
-    age,height,weight)
+    age, height, weight)
