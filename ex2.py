9e799703c5f75923f57bbffaba4f09d5ea2dea15
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 21:31:31 +0800

    注释和#号

--- ex2.py
+++ ex2.py
@@ -1,12 +1,12 @@
 #coding:utf-8
 #################
-#习题2:注释和#号
+# 习题2:注释和#号
 #################
 
-#这是注释的意思
-#任何在 #后面的东西都不会被python识别
-print "I could have code like this." #在代码的后面注释
-#打印"This won't run."
+# 这是注释的意思(#号后面加一个空格好看很多)
+# 任何在‘#’后面的东西都不会被python识别
+print "I could have code like this." # 在代码的后面注释
+# 打印"This won't run."
 print "This won't run."
 
-#注意倒着读代码有助于找错误
+# 注意倒着读代码有助于找错误
