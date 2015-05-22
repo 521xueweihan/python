954046554dbf7f9e1067da3c6804794bf5f72ca3
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 22:44:10 +0800

    提问

--- ex11.py
+++ ex11.py
@@ -1,14 +1,15 @@
 #coding:utf-8
 #################
-#习题11:提问
+# 习题11:提问
 #################
-#前言
+# 前言
 #
-#这里主要理解：接收用户的输入
+# 这里主要理解：接收用户的输入
 #
 
 print "How old are you?",
-age = raw_input()# 接收控制台的输入信息
+# 接收控制台的输入信息
+age = raw_input()
 print "How tall are you?",
 height = raw_input()
 print "How much do you weight?",
@@ -17,9 +18,7 @@ weight = raw_input()
 print "So,you're %r old, %r tall and %r heavy." %(
     age, height, weight)
 	
-#笔记	
-#
-#raw_input()是接受控制台输入的任何信息
-#
-#在print后面加“,”可以让输入在同一行
-#
+# 笔记	
+# 1.raw_input()是接受控制台输入的任何信息
+# 2.在print后面加“,”可以让输入在同一行
+
