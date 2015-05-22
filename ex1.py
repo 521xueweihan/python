e362ae58c3e6f02a4f6929f0ba3d65b13834e06f
xueweihan-ubuntu <595666367@qq.com>
2015-05-22 20:52:44 +0800

    第一个程序

--- ex1.py
+++ ex1.py
@@ -1,6 +1,7 @@
 #coding:utf-8
-print  "hello Word!"
+print "hello Word!"
 print  u"你好？"
+print "你好?"#在linux系统下不需要加u
 print "Heloo Again"
 print "I like typing this."
 print "This is fun."
@@ -9,5 +10,5 @@ print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 
 # 输出中文的时候要在字符串前面加u。
-# 因为python中字符串默认采用ASCII，要声明为unicode就要加u
-#print u"你好"
+# 因为python中字符串默认采用ASCII，要声明为unicode就要加u(linux环境下不需要加u)
+
