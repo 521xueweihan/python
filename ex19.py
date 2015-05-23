ff85f32d6c025b7ab6859eb29227bb0a97e7db5c
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 13:45:58 +0800

    函数和变量

--- ex19.py
+++ ex19.py
@@ -1,30 +1,30 @@
 #coding:utf-8
 #################
-#习题19:函数和变量
+# 习题19:函数和变量
 #################
-#前言
+# 前言
 #
-#注意：函数里的变量和脚本里的变量之间没有联系的
-#你可以用一下几种方式进行传参（给函数赋值）
+# 注意：函数里的变量和脚本里的变量之间没有联系的
+# 你可以用一下几种方式进行传参（给函数赋值）
 #
 
-#奶酪和饼干
+# 奶酪和饼干
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "You have %d cheeses!" % cheese_count
     print "You have %d boxes_of_crackers!" % boxes_of_crackers
     print "Man that's enough for a party!"
-    print "Get a blanket.\n"#毯子
+    print "Get a blanket.\n"    # 毯子
 	
 
 print u"We can just give the function numbers directly（直接的）:"
 cheese_and_crackers(20, 30)
 
 
-print "OR, we can use variables from our script:"#可以使用脚本中的变量给函数传参
+print "OR, we can use variables from our script:"    # 可以使用脚本中的变量给函数传参
 amount_of_cheese = 10
 amount_of_crackers = 50
 
-cheese_and_crackers(amount_of_cheese,amount_of_crackers)
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 
 print "We can even do math inside too:"
