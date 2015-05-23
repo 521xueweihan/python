40f22965951f3bf067f8065090da0981b7243864
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 13:51:57 +0800

    函数可以返回某些东西

--- ex21.py
+++ ex21.py
@@ -1,17 +1,17 @@
 #coding:utf-8
 #################
-#习题21:函数可以返回某些东西
+# 习题21:函数可以返回某些东西
 #################
-#前言
+# 前言
 #
-#函数使用return返回值
+# 函数使用return返回值
 #
 
 def add(a, b):
     print "ADDING %d +%d" % (a, b)
     return a + b
     
-#减法
+# 减法
 def subtract(a, b):
     print "SUBTRACTING %d - %d" % (a, b)    
     return a - b
@@ -31,13 +31,13 @@ height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
 
-print "Age: %d, Height: %d, Weight: %d, IQ: %d " % (age ,height ,weight ,iq)
+print "Age: %d, Height: %d, Weight: %d, IQ: %d " % (age, height, weight, iq)
 
 
-#A puzzle for the extra credit, type it in anyway.
+# A puzzle for the extra credit, type it in anyway.
 print "Here is a puzzle."
 
-#由内而外调用函数计算结果
+# 由内而外调用函数计算结果
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
-print "That becomes: ",what, "Can you do it by hand?"
+print "That becomes: ", what, "Can you do it by hand?"
