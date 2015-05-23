908371990e4b2d83143b2ccec1d0f46d1c416610
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 16:32:37 +0800

    模块，类和对象

---
+++ ex40.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+#################
+# 习题40:模块，类和对象
+#################
+# 前言
+# python是一门面向对象的语言。这个说法的意思是，python
+# 里面有一种叫做类（class）的结构。
+# 你需要明白的是：面向对象的思想，并尽快掌握它
+
+class Song(object):
+    
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+        self.name = xueweihan
+        
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+    
+    def screech(self):
+        print "-" * 5
+        print self.name
+            
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around the family",
+                        "With pockets full of shells"])
+                        
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+hi = song()
+hi.screech()
