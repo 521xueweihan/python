344553fa4ffb4f26e397df7757d0a1f7062a3225
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 15:04:39 +0800

    我自己写的一个游戏

--- ex36.py
+++ ex36.py
@@ -1,20 +1,22 @@
 #coding:utf-8
 #################
-#习题36:测试与调试
+# 习题36:测试与调试
 #################
-#前言
+# 前言
 #
-#家庭作业：
+# 家庭作业：
+# 写这个游戏，写的挺快的
+# 就是后期遇到了个逻辑bug
+# 用了很长时间才铲除掉
 #
-#写这个游戏，写的挺快的
-#就是后期遇到了个逻辑bug
-#用了很长时间才铲除掉
+# 好了，let play game吧
 #
-#好了，let play game吧
 
-import random#导入随机数
+import random    # 导入随机数
+
 print ("-"*5) + (u"更多练习") +("-"*5)
-#选择武器并跳转到challeng方法
+
+# 选择武器并跳转到challeng方法
 def weapon():
     treasure = "none"
     print u"选一个你喜欢的武器吧！"
@@ -34,8 +36,9 @@ def weapon():
         print u"你选的啥？重来！"
         print "-----------"
         weapon()
-#选择挑战的内容，treasure参数是宝物的值：none和boom
-#boom 只能在dog房打到，有了boom才能打死bos
+        
+# 选择挑战的内容，treasure参数是宝物的值：none和boom
+# boom只能在dog房打到，有了boom才能打死bos
 def challenge(treasure):   
     print u"你打算挑战那个？"
     print """
@@ -53,19 +56,20 @@ def challenge(treasure):
         print "-----------"
         challenge(treasure)
         
-#爆宝的方法，利用随机函数random，随机产生boom或者none，并把treasure参数传给challenge
+# 爆宝的方法，利用随机函数random，随机产生boom或者none，并把treasure参数传给challenge
 def get_treasure():
 
-    treasure = random.choice(["boom","none"])
+    treasure = random.choice(["boom","none"])    # 随机产生boom或者none
     
     if treasure == "boom":
         print u"恭喜!你获得了无敌的存在：核弹.\n咔咔咔......\n 你装备上了核弹，可以去秒杀bos了!"    
         challenge(treasure)
-        #return treasure  #就是这里出错了!!!不需要return 直接把treasure的值给challenge就好了         
+        #return treasure  # 就是这里出错了!!!不需要return 直接把treasure的值给challenge就好了         
     else:
         print u"你什么都没有获得！\n"
         challenge(treasure)
-#dog_room方法           
+        
+# dog_room方法           
 def dog_room():
     treasure = "none"
     print u"这里有只恶狗！"
@@ -78,7 +82,8 @@ def dog_room():
         get_treasure()        
     else:
         challenge(treasure)
-#bos_room方法   
+        
+# bos_room方法   
 def bos_room(treasure):
     print treasure
     print u"你面前是一个大bos！！十分大"
@@ -92,8 +97,9 @@ def bos_room(treasure):
         print u"你逃跑了"
         challenge(treasure)
     else:
-        challenge(treasure)
-#判断是否胜利：传入参数为treasure，如果为boom：胜利 否则失败跳转到weapon       
+        challenge(treasure)  
+             
+# 判断是否胜利：传入参数为treasure，如果为boom：胜利 否则失败跳转到weapon       
 def win(treasure):
     if treasure == "boom":
         print u"你用核弹秒杀了bos！"
@@ -103,12 +109,12 @@ def win(treasure):
         print u"你被bos打死了，快去dog房刷武器吧！"
         weapon()
         
-weapon()#游戏开始！
+weapon()    # 游戏开始！
 
-#笔记
+# 笔记
 #
-#作者在书上给了一些很好的编程习惯，
-#我去整理一下再放过来！
+# 作者在书上给了一些很好的编程习惯，
+# 我去整理一下再放过来！
 #
 ######
 # If语句的规则：
