3c106d4c08016bba1b72436e626ddc3b44c4664c
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 13:31:21 +0800

    读取文件

--- ex15.py
+++ ex15.py
@@ -1,11 +1,11 @@
 #coding:utf-8
 #################
-#习题15:读取文件
+# 习题15:读取文件
 #################
-#前言
+# 前言
 #
-#学习：如何读取本地文件（ex15_sample.txt）
-#
+# 学习：如何读取本地文件（ex15_sample.txt）
+# 
 
 from sys import argv
 
@@ -15,8 +15,8 @@ txt = open(filename)
 
 print "Here's your file %r:" % filename
 print txt.read()
-#txt.close()#处理完文件后将他关闭
-#print txt.read()#测试是否关闭了
+# txt.close()#处理完文件后将他关闭
+# print txt.read()#测试是否关闭了
 
 
 print "Type the  filename again:"
@@ -26,10 +26,9 @@ txt_again = open(file_again)
 
 print txt_again.read()
 
-#笔记
+# 笔记
 #
-#两种方法读取文件
-#
-#1.得到文件名称——两种方法得到文件名
-#2.open（）打开文件
-#3.read（）输出文件
+# 两种方法读取文件
+# 1.得到文件名称——两种方法得到文件名
+# 2.open（）打开文件
+# 3.read（）输出文件
