89d69cd1b2b2bf486edf621abf7fede0f212599e
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 15:00:07 +0800

    分支和函数

--- ex35.py
+++ ex35.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 #################
-#习题35:分支和函数
+# 习题35:分支和函数
 #################
-#前言
+# 前言
 #
-#就是逻辑和函数的综合应用，有点长不要被吓到
+# 就是逻辑和函数的综合应用，有点长不要被吓到
 #
 
 from sys import exit
@@ -13,17 +13,17 @@ def gold_room():
     print u"这个房间装满了黄金，你要拿走多少？"
     
     next = raw_input("> ")
-    if "0" in next or "1" in next:#检测输入中是否有0或1
-    #if int(next) >= 0:#检测输入是正数
+    if "0" in next or "1" in next:    # 检测输入中是否有0或1
+    #if int(next) >= 0:    # 检测输入是正数
         how_much = int(next)
     else:
-        dead(u"Man,请注意数据类型")
+        dead(u"Man, 请注意数据类型")
     
     if how_much < 50:
-        print "Nice ,you're not greedy, you win!"
+        print "Nice, you're not greedy, you win!"
         exit(0)
     else:
-        dead("You gready bastard!")#贪婪的混蛋
+        dead("You gready bastard!")    # 贪婪的混蛋
     
     
 def bear_room():
@@ -37,8 +37,8 @@ def bear_room():
         next = raw_input("> ")
         
         if next == "take honey":
-            dead("The bear looks at you then slaps your face off。")#slap:掌击
-        elif next == "taunt bear" and not bear_moved:#你输入：嘲讽熊，别且熊移动过来
+            dead("The bear looks at you then slaps your face off。")   # slap:掌击
+        elif next == "taunt bear" and not bear_moved:    # 你输入：嘲讽熊，别且熊移动过来
             print "The bear has moved from the door. You can go through it now."
             bear_moved = True
         elif next == "taunt bear" and bear_moved:
@@ -51,7 +51,7 @@ def bear_room():
             
 def cthulhu_room():
     print "Here you see the great evil Cthulhu."
-    print "He,it ,whatever stares at you and you go insane."#邪神的眼睛在注视着你，你逃跑了
+    print "He,it ,whatever stares at you and you go insane."    # 邪神的眼睛在注视着你，你逃跑了
     print "Do you flee your life or eat your head?"
     
     next = raw_input("> ")
@@ -65,7 +65,7 @@ def cthulhu_room():
         
         
 def dead(why):
-    print why,"God job!"
+    print why, "God job!"
     exit(0)
     
 def start():
@@ -83,13 +83,10 @@ def start():
         dead("You stumble around the room until you starve.")
         
         
-start()#书上的游戏
+start()    # 调用start（）函数
 
-#更多练习    
-
-
-#笔记
+# 笔记
 #
-#字符串前面加u，对该字符串毫无影响
-#但是输入的时候还是有问题（raw_input()）
+# 字符串前面加u，对该字符串毫无影响
+# 但是输入的时候还是有问题（raw_input()）
 #
