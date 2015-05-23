40ac46ea04f428577506ed44a917c72112f146ac
xueweihan-ubuntu <595666367@qq.com>
2015-05-23 13:37:31 +0800

    更多文件操作

--- ex17.py
+++ ex17.py
@@ -1,12 +1,11 @@
 #coding:utf-8
 #################
-#习题17:更多文件操作
+# 习题17:更多文件操作
 #################
-#前言
+# 前言
 #
-#编写一个python脚本：
-#
-#实现复制文件
+# 编写一个python脚本：
+# 实现复制文件
 #
 
 from sys import argv
@@ -20,13 +19,13 @@ print "Copying from %s to %s" % (from_file, to_file)
 in_file = open(from_file)
 indata = in_file.read()
 
-print "The input file is %d bytes long" % len(indata)#这个文件多大
+print "The input file is %d bytes long" % len(indata)    #这个文件多大
 
-print "Does the output file exist? %r" % exists(to_file)#输出的文件是否存在
-print "Ready , hit RETURN to continue,CTRL-C to abort."
+print "Does the output file exist? %r" % exists(to_file)    #输出的文件是否存在
+print "Ready , hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
-out_file = open(to_file, 'w')#这里需要注意下写的模式下不可以读
+out_file = open(to_file, 'w')    # 这里需要注意下写的模式下不可以读
 out_file.write(indata)
 
 print "Alright, all done."
