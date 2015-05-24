c5fd065b7003ef99e08966eec815b4eb8bba530a
xueweihan-ubuntu <595666367@qq.com>
2015-05-24 21:11:35 +0800

    该文件用于练习如果应用类

---
+++ classTest.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+#################
+# 练习应用类
+#################
+
+class Test:
+    def __init__(self,name):
+        self.name = name
+        
+x = Test(1)    
+print x.name   这里尝试输出‘你好’————把第十行改成：x = Test(u'你好')
+
+
