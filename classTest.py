1a78d3da07d9357a97b24e9ec0342954a4a24ba2
xueweihan-ubuntu <595666367@qq.com>
2015-05-24 22:42:59 +0800

    用于练习类

--- classTest.py
+++ classTest.py
@@ -25,9 +25,9 @@ print 'two:',two.name, ';Same thing:', two.samething
 ###################
 
 class TestError:
-    tricks = []
-#    def __init__(self,name)：
-#        self.name = name
+    tricks = []    #正确的应该是把这句移动__init__函数中或者add_tricks函数中
+#   def __init__(self)：
+#       
         
     def add_tricks(self,stuff):
         self.tricks.append(stuff)
@@ -36,7 +36,8 @@ dog1 = TestError()
 dog1.add_tricks('dog1')
 dog2 = TestError()
 dog2.add_tricks('dog2')
-print TestError.tricks    # 输出之而后你会发现tricks是共享的列表，对象的数据变成共享的了
+print dog1.tricks,'\n----------'
+print dog2.tricks    # 输出之而后你会发现tricks是共享的列表，对象的数据变成共享的了
 # 这里需要注意的是：类中属性名和方法名要区别开否则会出现很难发现的错误
-
+# 再次声明命名约定可以避免很多麻烦。
 ####################
