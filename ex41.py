83cbf0b3769da1a203b6a994599475818533a0c9
xueweihan-ubuntu <595666367@qq.com>
2015-05-25 14:22:29 +0800

    学习面向对象术语

--- ex41.py
+++ ex41.py
@@ -36,4 +36,55 @@ if len(sys.argv) == 2 and sys.argv[1] == "english":
 
 # load up the words from the website
 for word in urlopen(WORD_URL).readlines():
-    WORD.append(word.strip())
+    WORDS.append(word.strip())
+    
+
+def convert(snippet, phrase):    # 转换
+    class_names = [w.capitalize() for w in
+                   random.sample(WORDS, snippet.count("%%%"))]   #
+    other_names = random.sample(WORDS, snippet.count("***"))    #
+    results = []
+    param_names = []
+    
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1, 3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))    #
+        
+    for sentence in snippet, phrase:
+        result = sentence[:]    #
+        
+    # fake class names (篡改)
+    for word in class_names:
+        result = result.replace("%%%", word, 1)
+        
+    # fake other names
+    for word in other_names:
+        result = result.replace("***", word, 1)
+        
+    # fake parameter lists
+    for word in param_names:
+        result = result.replace("@@@", word, 1)
+        
+    results.append(result)
+    
+    return results
+    
+    
+# keep going until they hit CTRL-D
+try:
+    while True:
+        snippets = PHRASES.keys()
+        random.shuffle(snippets)    # 洗牌
+        
+        for snippet in snippets:
+            phrase = PHRASES[snippet]
+            question, answer = convert(snippet, phrase)
+            if PHRASE_FIRST:
+                question, answer = answer, question
+                
+            print question
+            
+            raw_input("> ")
+            print "ANSWER: %s\n\n" % answer
+except EOFError:
+    print "\nBye"
