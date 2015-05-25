7fc2aa82b4706b9caf5945aa392fac59c3ef4b6e
xueweihan-ubuntu <595666367@qq.com>
2015-05-25 22:45:48 +0800

    一个for中遍历两列表

--- how_for_two_list.py
+++ how_for_two_list.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 #################
+# for循环两个列表的过程
 list1 = ['1', '1']
 list2 = ['A','B']
 
@@ -22,7 +23,7 @@ yw = dir.values()
 # 这段代码是遍历两个列表之后赋值给新的两个列表
 for i in xq, yw:
     answer, question = i[:]    # 这里可以加代码对元素操作之后再赋值给新的列表
-    print answer, ",", question  
+    print answer, ",", question  # 你可以把这句放在for循环外面在看下结果
 
 print "---------"
 # 复杂版
