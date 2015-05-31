2762a8763119d3f20f2ec9712b839d9e700ef130
xueweihan <595666367@qq.com>
2015-05-31 22:58:23 +0800

    ex50 第一个网站

---
+++ gothonweb/bin/app.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+import web
+
+urls = (
+    '/', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')   # 渲染templates/文件夹下的index
+
+class Index(object):
+    def GET(self):
+        greeting = "Hello World1"
+        #return render.index(greeting = greeting # 把greeting变量传递给index网页模板中的greeting
+        return render.foo(greeting = greeting)  #templates文件夹下创建了一个新的foo.html的模板
+if __name__ == "__main__":
+    app.run()
