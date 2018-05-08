5dbd7d65a226105ce7ef0486c282850c2292aa09
Chris <chriszhu1001@gmail.com>
2018-05-08 04:22:20 -0400

    Update ex1.py
    Xiaojiejie, banben shengji le. Print geshi ye yao gai gai le

--- content/ex1.py
+++ content/ex1.py
@@ -2,15 +2,15 @@
 #################
 # 习题1:第一个程序
 #################
-print "hello Word!"
-print  u"你好？"
-print "你好?"#在linux系统下不需要加u
-print "Heloo Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+print("hello Word!")
+print(u"你好？")
+print("你好?")#在linux系统下不需要加u
+print("Heloo Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
 
 # 输出中文的时候要在字符串前面加u。
 # 因为python中字符串默认采用ASCII，要声明为unicode就要加u(linux环境下不需要加u)
