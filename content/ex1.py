ca7c407944ef9c531f03aa4ccb054cbfaf27c795
cmzysk <44422130+cmzysk@users.noreply.github.com>
2018-10-26 10:18:40 +0800

    ysk_test
    add something easy

--- content/ex1.py
+++ content/ex1.py
@@ -12,6 +12,7 @@ print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 
+print 'this is ysk'
 # 输出中文的时候要在字符串前面加u。
 # 因为python中字符串默认采用ASCII，要声明为unicode就要加u(linux环境下不需要加u)
 
